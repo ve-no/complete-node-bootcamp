f475cccb1877ef7477fa6cdd45fd046883cbbdbe
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-03 00:22:19 +0100

    just a commit

--- 4-natours/controllers/handlerFactory.js
+++ 4-natours/controllers/handlerFactory.js
@@ -9,6 +9,7 @@ exports.deleteOne = Model =>
     if (!doc) {
       return next(new AppError('No document found with that ID', 404));
     }
+    
 
     res.status(204).json({
       status: 'success',
