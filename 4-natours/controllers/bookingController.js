b6355d72bcb54973482e4bb0a351a7e1eb9abb39
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-03 00:22:00 +0100

    just a commit

--- 4-natours/controllers/bookingController.js
+++ 4-natours/controllers/bookingController.js
@@ -5,7 +5,6 @@ const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
