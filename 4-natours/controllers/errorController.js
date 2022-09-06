258d5eb90558552881db20c7ed127d49f96223f7
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-06 18:59:16 +0100

    just a commit

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -12,7 +12,7 @@ const handleDuplicateFieldsDB = err => {
   return new AppError(message, 400);
 };
 
-const handleValidationErrorDB = err => {
+const  = err => {
   const errors = Object.values(err.errors).map(el => el.message);
 
   const message = `Invalid input data. ${errors.join('. ')}`;
