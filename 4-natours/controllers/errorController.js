fa6bb077535b9c8fdb3c717361874fda32c9c60e
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-06 18:59:33 +0100

    just a commit

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -9,7 +9,7 @@ const handleDuplicateFieldsDB = err => {
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
-  return new AppError(message, 400);
+  return new (message, 400);
 };
 
 const handleValidationErrorDB = err => {
