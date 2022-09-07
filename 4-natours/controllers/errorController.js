19f11b49c518aaf92caa91972332477a991cadc1
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-07 01:03:18 +0100

    just a commit

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -74,7 +74,7 @@ const sendErrorProd = (err, req, res) => {
   }
   // B) Programming or other unknown error: don't leak error details
   // 1) Log error
-  // console.error('ERROR 💥', err);
+  console.error('ERROR 💥', err);
   // 2) Send generic message
   return res.status(err.statusCode).render('error', {
     title: 'Something went wrong!',
