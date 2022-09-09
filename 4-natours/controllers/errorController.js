8a24ef57ab6f399f938a834aa0b193e4c12a04b0
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-09 19:24:23 +0100

    just a commit

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -72,6 +72,7 @@ const sendErrorProd = (err, req, res) => {
       msg: err.message
     });
   }
+  
   // B) Programming or other unknown error: don't leak error details
   // 1) Log error
   console.error('ERROR 💥', err);
