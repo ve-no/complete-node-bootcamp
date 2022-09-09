44633617bae682abc101e42a12ae2176e14df90d
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-09 19:25:21 +0100

    just a commit

--- 4-natours/controllers/handlerFactory.js
+++ 4-natours/controllers/handlerFactory.js
@@ -79,7 +79,6 @@ exports.getAll = Model =>
     // const doc = await features.query.explain();
     const doc = await features.query;
 
-    
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
