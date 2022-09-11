58a868c18656662aa136b7dd817c06d665eb4be5
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-11 01:23:03 +0100

    just a commit

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -64,7 +64,7 @@ const sendErrorProd = (err, req, res) => {
     });
   }
 
-  // B) RENDERED WEBSITE
+  B) RENDERED WEBSITE
   // A) Operational, trusted error: send message to client
   if (err.isOperational) {
     return res.status(err.statusCode).render('error', {
