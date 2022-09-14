75713ba465fbf5cb875e8efa24f362a122ce1ab5
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-14 22:52:01 +0100

    just a commit

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -6,7 +6,6 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const compression = require('compression');
