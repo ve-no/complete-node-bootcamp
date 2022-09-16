2e03264b209c342bd2c48ad36b9c5c93a5c78c62
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-16 21:54:15 +0100

    just a commit

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -32,7 +32,6 @@ app.set('views', path.join(__dirname, 'views'));
 // Implement CORS
 app.use(cors());
 // Access-Control-Allow-Origin *
-
 // api.natours.com, front-end natours.com
 // app.use(cors({
 //   origin: 'https://www.natours.com'
