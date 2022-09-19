1025ca76a80f5cacbc15ae32064eb403a19c7c97
Allal EL BAKOURI <allal.elbakourii@gmail.com>
2022-09-19 22:01:09 +0100

    just  a commit

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -29,6 +29,7 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+
 // Implement CORS
 app.use(cors());
 // Access-Control-Allow-Origin *
